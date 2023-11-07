7fbd082e46eb4f80fe2664ce954293c9b953c84d
Watthanachai Dueanklang <bosskillerz@gmail.com>
2023-11-07 10:30:21 +0700

    Changes to be committed:

--- README.md
+++ README.md
@@ -11,7 +11,42 @@ cd gensudoku-rs
 cargo build
 
 ```
+```tex
 
+\documentclass[a4paper]{article}
+\usepackage{geometry}
+\usepackage{sudoku}
+\begin{document}
+
+
+\noindent \verb|N_BLANKSEED = 2815627944, SBID = 2815627944| \verb|N = 0, SN_BLANK = 35, SD = 5| \newline \setlength\sudokusize{15cm}
+\begin{sudoku}
+| | | | |4| |3|1|6|.
+|4|8| | | | |5| | |.
+|1|6| |5|7| | | |4|.
+|8| |6| | |3|7|5|9|.
+|3|1|9| | |5|6|4| |.
+|5| | |8|9| |1|3|2|.
+|2| |8|1| | |9| |3|.
+|6|9|1|3| | | | |5|.
+|7|3|5| |6|2| | |1|.
+\end{sudoku}
+\newpage
+
+\noindent \verb|N_BLANKSEED = 2815627945, SBID = 2815627945| \verb|N = 1, SN_BLANK = 36, SD = 5| \newline \setlength\sudokusize{15cm}
+\begin{sudoku}
+| |8|4|3| | |6|9| |.
+|3|1|6| | |5| | |2|.
+|9|7| |8| |2|3| |4|.
+|8|6|2| | | | |5| |.
+| | | |6| |4| |7| |.
+| |4|3|2|5| | | |1|.
+| |9| |4|2| | |3|7|.
+|4| |7|5|8|6| |2|9|.
+| |2| | |9|3|8|4| |.
+\end{sudoku}
+
+```
 
 ## Contact me
 
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ extern crate sudoku_rs;
 use sudoku_rs::prelude::*;
 mod def;
 
+extern crate clap;
+use clap::{arg, value_parser, ArgAction};
+
 use std::io::Write;
 
 fn print_board_tex(
@@ -59,12 +62,95 @@ fn print_board_tex(
     Ok(())
 }
 
+fn parse_command_line(
+    def_nbseed: sudoku_sys::URND32,
+    def_sbid: sudoku_sys::URND32,
+    def_nblank: u32,
+    def_sd: u32,
+    def_nboard: u32,
+) -> (sudoku_sys::URND32, sudoku_sys::URND32, u32, u32, u32) {
+    let matches = clap::Command::new("gensudoku-rs")
+        .arg(
+            arg!(--nbseed <NBSEED> "A seed for RNG")
+                .required(false)
+                .value_parser(value_parser!(sudoku_sys::URND32))
+                .action(ArgAction::Set),
+        )
+        .arg(
+            arg!(--sbid <SBID> "A board ID")
+                .value_parser(value_parser!(sudoku_sys::URND32))
+                .action(ArgAction::Set),
+        )
+        .arg(
+            arg!(--nblank <NBLANK> "Mean of number of blank of sudoku board")
+                .value_parser(value_parser!(u32))
+                .action(ArgAction::Set),
+        )
+        .arg(
+            arg!(--sd <SD> "Standard Deviation of nblank")
+                .value_parser(value_parser!(u32))
+                .action(ArgAction::Set),
+        )
+        .arg(
+            arg!(--nboard <NBOARD> "Number of sudoku board")
+                .value_parser(value_parser!(u32))
+                .action(ArgAction::Set),
+        )
+        .get_matches();
+
+    let nbseed = if let Some(val) = matches.get_one::<sudoku_sys::URND32>("nbseed") {
+        *val
+    } else if matches.contains_id("nbseed") {
+        panic!("Parsing nbseed!!");
+    } else {
+        def_nbseed
+    };
+
+    let sbid = if let Some(val) = matches.get_one::<sudoku_sys::URND32>("sbid") {
+        *val
+    } else if matches.contains_id("sbid") {
+        panic!("Parsing sbid!!");
+    } else {
+        def_sbid
+    };
+
+    let nblank = if let Some(val) = matches.get_one::<sudoku_sys::URND32>("nblank") {
+        *val
+    } else if matches.contains_id("nblank") {
+        panic!("Parsing nblank!!");
+    } else {
+        def_nblank
+    };
+
+    let sd = if let Some(val) = matches.get_one::<sudoku_sys::URND32>("sd") {
+        *val
+    } else if matches.contains_id("sd") {
+        panic!("Parsing sd!!");
+    } else {
+        def_sd
+    };
+
+    let nboard = if let Some(val) = matches.get_one::<sudoku_sys::URND32>("nboard") {
+        *val
+    } else if matches.contains_id("nboard") {
+        panic!("Parsing nboard!!");
+    } else {
+        def_nboard
+    };
+
+    (nbseed, sbid, nblank, sd, nboard)
+}
+
 fn main() -> std::io::Result<()> {
-    let nbseed = sudoku_rs::seed_from_entropy();
-    let sbid = nbseed;
-    let nblank = def::NBLANK;
-    let sd = def::SD;
-    let nboard = def::NBOARD;
+    let entropy_number = sudoku_rs::seed_from_entropy();
+
+    let (nbseed, sbid, nblank, sd, nboard) = parse_command_line(
+        entropy_number,
+        entropy_number,
+        def::NBLANK,
+        def::SD,
+        def::NBOARD,
+    );
 
     let mut game = sudoku_sys::sgs_game::new(0, nblank);
     print_board_tex(
